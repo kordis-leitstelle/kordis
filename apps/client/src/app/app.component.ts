c93d4d2ae08324232bd4cfb31f0b56d7de185174
Timon Masberg <contact@timonmasberg.com>
2022-12-24 01:06:09 +0100

    chore: change component prefix to krd

--- apps/client/src/app/app.component.ts
+++ apps/client/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-	selector: 'kordis-root',
+	selector: 'krd-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.css'],
 })
--- apps/client/src/app/nx-welcome.component.ts
+++ apps/client/src/app/nx-welcome.component.ts
@@ -3,7 +3,7 @@ import { Component, ViewEncapsulation } from '@angular/core';
 /* eslint-disable */
 
 @Component({
-	selector: 'kordis-nx-welcome',
+	selector: 'krd-nx-welcome',
 	template: `
 		<!--
      * * * * * * * * * * * * * * * * * * * * * * * * * * * *
