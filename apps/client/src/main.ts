0feb223efa1059ad872c21955b191ec7eab13ebd
Timon Masberg <contact@timonmasberg.com>
2023-02-02 16:47:28 +0100

    chore: minor adjustments

--- apps/client/src/main.ts
+++ apps/client/src/main.ts
@@ -4,4 +4,5 @@ import { AppModule } from './app/app.module';
 
 platformBrowserDynamic()
 	.bootstrapModule(AppModule)
+	// eslint-disable-next-line no-console
 	.catch((err) => console.error(err));
