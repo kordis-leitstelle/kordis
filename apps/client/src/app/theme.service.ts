f1c491d6e0c41f43577d97752584dc9ff589bd54
Timon Masberg <contact@timonmasberg.com>
2023-02-03 00:59:31 +0100

    fix: theme service only applying to first level nodes

--- apps/client/src/app/theme.service.ts
+++ apps/client/src/app/theme.service.ts
@@ -1,13 +1,23 @@
-import { Injectable } from '@angular/core';
-import { Observable, scan, shareReplay, startWith, Subject } from 'rxjs';
+import { Injectable, OnDestroy } from '@angular/core';
+import {
+	Observable,
+	ReplaySubject,
+	scan,
+	shareReplay,
+	skip,
+	startWith,
+	Subject,
+	takeUntil,
+} from 'rxjs';
 
 @Injectable({
 	providedIn: 'root',
 })
-export class ThemeService {
-	isDark$: Observable<boolean>;
+export class ThemeService implements OnDestroy {
+	readonly isDark$: Observable<boolean>;
 
 	private readonly toggleDarkModeSubject = new Subject<void>();
+	private readonly ngDestroySubject = new ReplaySubject(1);
 
 	constructor() {
 		this.isDark$ = this.toggleDarkModeSubject.pipe(
@@ -15,9 +25,30 @@ export class ThemeService {
 			startWith(false),
 			shareReplay({ bufferSize: 1, refCount: true }),
 		);
+		this.startWatchingDarkModeState();
+	}
+
+	ngOnDestroy(): void {
+		this.ngDestroySubject.next(true);
+		this.ngDestroySubject.complete();
 	}
 
 	toggleDarkMode(): void {
 		this.toggleDarkModeSubject.next();
+	}
+
+	private startWatchingDarkModeState(): void {
+		const htmlTag = document
+			.getElementsByTagName('html')
+			.item(0) as HTMLHtmlElement;
+		this.isDark$
+			.pipe(skip(1), takeUntil(this.ngDestroySubject))
+			.subscribe((isDark) => {
+				if (isDark) {
+					htmlTag.classList.add('dark');
+				} else {
+					htmlTag.classList.remove('dark');
+				}
+			});
 	}
 }
