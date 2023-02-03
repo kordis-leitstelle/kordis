b2b8c934fd1826baa6903e1a718a7bcfded3b9af
Timon Masberg <contact@timonmasberg.com>
2023-02-03 23:55:25 +0100

    feat(frontend-shared-ui): add input

--- libs/frontend/shared/ui/src/lib/input/input.component.ts
+++ libs/frontend/shared/ui/src/lib/input/input.component.ts
@@ -13,60 +13,20 @@ import {
 } from '@angular/forms';
 
 /* eslint @typescript-eslint/no-explicit-any: 0 */
+/* eslint-disable @typescript-eslint/no-empty-function, @typescript-eslint/no-unused-vars */
+
 @Component({
 	selector: 'krd-input',
 	standalone: true,
 	imports: [CommonModule, FormsModule],
-	template: `
-		<div>
-			<label *ngIf="label">{{ label }}</label>
-			<div class="relative mt-1 rounded-md shadow-sm">
-				<input
-					[type]="type"
-					[placeholder]="placeholder"
-					[disabled]="isDisabled"
-					[(ngModel)]="value"
-					[class.input-error]="hasError"
-				/>
-				<div
-					*ngIf="hasError"
-					class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3"
-				>
-					<svg
-						class="h-5 w-5 text-red-500"
-						xmlns="http://www.w3.org/2000/svg"
-						viewBox="0 0 20 20"
-						fill="currentColor"
-						aria-hidden="true"
-					>
-						<path
-							fill-rule="evenodd"
-							d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-5a.75.75 0 01.75.75v4.5a.75.75 0 01-1.5 0v-4.5A.75.75 0 0110 5zm0 10a1 1 0 100-2 1 1 0 000 2z"
-							clip-rule="evenodd"
-						/>
-					</svg>
-				</div>
-			</div>
-			<p
-				*ngIf="errorText && hasError"
-				class="mt-2 mr-3 float-right text-sm text-red-600"
-			>
-				{{ errorText }}
-			</p>
-		</div>
-	`,
+	templateUrl: './input.component.html',
 	styles: [
 		`
-			label {
-				@apply block font-medium text-gray-700;
-			}
-
-			input {
-				@apply block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm;
-			}
-
 			.input-error {
-				@apply border-red-300 text-red-900 placeholder-red-300 focus:border-red-500 focus:ring-red-500;
+				@apply border-red-300 text-red-900 placeholder-red-300 focus:border-danger focus:ring-danger;
+			}
+			:host-context(.dark) .input-error {
+				@apply border-red-400  text-danger placeholder-red-400;
 			}
 		`,
 	],
@@ -86,8 +46,6 @@ export class InputComponent implements ControlValueAccessor {
 	@Input() type: 'text' | 'email' | 'password' = 'text';
 	@Input() hasError = false;
 	@Input() errorText?: string;
-	private onTouched!: () => void;
-	private onChanged!: (_: string) => void;
 
 	constructor(private readonly cd: ChangeDetectorRef) {}
 
@@ -115,7 +73,11 @@ export class InputComponent implements ControlValueAccessor {
 		this._value = v;
 		this.cd.detectChanges();
 	}
+
 	setDisabledState?(isDisabled: boolean): void {
 		this.isDisabled = isDisabled;
 	}
+
+	private onTouched: () => void = () => {};
+	private onChanged: (_: string) => void = (_) => {};
 }
