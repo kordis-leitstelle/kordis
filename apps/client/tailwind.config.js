aac3aec82a55b0ad1deb865b4f46a2a6773b1a82
Timon Masberg <contact@timonmasberg.com>
2023-02-03 20:58:51 +0100

    feat(frontend-shared-ui): allow only specific colors

--- apps/client/tailwind.config.js
+++ apps/client/tailwind.config.js
@@ -1,13 +1,31 @@
 const { createGlobPatternsForDependencies } = require('@nrwl/angular/tailwind');
 const { join } = require('path');
+const colors = require('tailwindcss/colors');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: [
-		join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
+		join(__dirname, 'src/**/!(*.spec).{ts,html}'),
 		...createGlobPatternsForDependencies(__dirname),
 	],
 	theme: {
+		colors: {
+			transparent: 'transparent',
+			current: 'currentColor',
+			white: colors.white,
+			black: colors.black,
+			red: colors.red,
+			blue: colors.blue,
+			gray: colors.gray,
+			green: colors.green,
+			yellow: colors.yellow,
+			primary: colors.blue['500'],
+			'primary-dark': colors.blue['700'],
+			secondary: colors.gray['400'],
+			'secondary-dark': colors.gray['600'],
+			danger: colors.red['500'],
+			'danger-dark': colors.red['600'],
+		},
 		extend: {},
 	},
 	darkMode: 'class',
