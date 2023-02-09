405a700a793877172bc286fd508c9a3153530929
Timon Masberg <contact@timonmasberg.com>
2023-02-09 21:09:48 +0100

    feat(api): boostrap api with e2e project

---
+++ apps/api-e2e/src/api/api.spec.ts
@@ -0,0 +1,27 @@
+import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppModule } from '@kordis/api';
+import request from 'supertest';
+
+describe('GET /', () => {
+	let app: INestApplication;
+
+	beforeAll(async () => {
+		const moduleFixture: TestingModule = await Test.createTestingModule({
+			imports: [AppModule],
+		}).compile();
+
+		app = moduleFixture.createNestApplication();
+		await app.init();
+	});
+
+	afterAll(async () => {
+		await app.close();
+	});
+
+	it('should return a message', async () => {
+		return request(app.getHttpServer())
+			.get('/')
+			.expect(200, JSON.stringify({ message: 'Welcome to api!' }));
+	});
+});
