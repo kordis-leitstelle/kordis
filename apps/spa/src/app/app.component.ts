658d7a16fdea879aaaa57170c0cf270529ac13e2
Timon Masberg <contact@timonmasberg.com>
2023-02-14 00:54:29 +0100

    chore: fix code smells

--- apps/spa/src/app/app.component.ts
+++ apps/spa/src/app/app.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
 	selector: 'kordis-root',
 	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
 	title = 'client';
