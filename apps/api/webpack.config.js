35e4cae425e720aecfa6b0bbd528f3360caef591
Timon Masberg <contact@timonmasberg.com>
2023-02-17 12:16:06 +0100

    chore: migrate to latest nx

--- apps/api/webpack.config.js
+++ apps/api/webpack.config.js
@@ -2,7 +2,5 @@ const { composePlugins, withNx } = require('@nrwl/webpack');
 
 // Nx plugins for webpack.
 module.exports = composePlugins(withNx(), (config) => {
-	// Update the webpack config as needed here.
-	// e.g. `config.plugins.push(new MyPlugin())`
 	return config;
 });
