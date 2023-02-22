d35c34dbdbfea48e0b64ad81d7463df28f73d162
Timon Masberg <contact@timonmasberg.com>
2023-02-22 16:57:14 +0100

    chore(api): enable cors

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -9,7 +9,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
+	const app = await NestFactory.create(AppModule, { cors: true });
 	const port = process.env.PORT || 3333;
 	await app.listen(port);
 	Logger.log(`🚀 Application is running on: http://localhost:${port}}`);
