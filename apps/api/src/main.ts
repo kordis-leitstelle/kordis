d37e0051403b47c2cc2f5043bcd085e57b99ca57
Timon <contact@timonmasberg.com>
2023-02-25 10:40:16 +0100

    chore: add eslint rules (#59)
    * chore: add no console, explicit function return type eslint rule
    
    * chore: add rxjs angular, introduce eslint config for angular

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,13 +2,12 @@
  * This is not a production server yet!
  * This is only a minimal backend to get started.
  */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
 	const app = await NestFactory.create(AppModule, { cors: true });
 	const port = process.env.PORT || 3333;
 	await app.listen(port);
