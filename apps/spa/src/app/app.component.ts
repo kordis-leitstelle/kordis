52235794d50d9cd820a27cfa57dd289139e129e6
Timon Masberg <contact@timonmasberg.com>
2023-03-14 19:22:38 +0100

    feat(spa): implement auth library in spa app

--- apps/spa/src/app/app.component.ts
+++ apps/spa/src/app/app.component.ts
@@ -1,14 +1,52 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable, delay, filter, first } from 'rxjs';
 
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { AuthService } from '@kordis/spa/auth';
+
+import { environment } from '../environments/environment';
 
 @Component({
 	selector: 'kordis-root',
-	standalone: true,
-	imports: [NxWelcomeComponent],
-	template: `<kordis-nx-welcome></kordis-nx-welcome>`,
+	template: `
+		<ng-container *ngIf="authInitCompleted$ | async; else loading">
+			<router-outlet></router-outlet>
+		</ng-container>
+		<ng-template #loading>
+			<div
+				data-testid="loading-indicator"
+				class="flex h-screen items-center justify-center"
+			>
+				<div
+					class="inline-block h-28 w-28 animate-spin rounded-full border-8 border-solid border-current border-r-transparent align-[-0.125em] text-blue-500 motion-reduce:animate-[spin_1.5s_linear_infinite]"
+					role="status"
+				>
+					<span
+						class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+						>Laden...</span
+					>
+				</div>
+			</div>
+		</ng-template>
+	`,
+
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-	title = 'client';
+	readonly authInitCompleted$: Observable<boolean> =
+		this.authService.isDoneLoading$.pipe(
+			filter((isDone: boolean) => isDone),
+			delay(300), // looks a bit cleaner
+			first(),
+		);
+
+	constructor(
+		private readonly authService: AuthService,
+		private readonly router: Router,
+	) {
+		authService.init(
+			environment.oauth.config,
+			environment.oauth.discoveryDocumentUrl,
+		);
+	}
 }
