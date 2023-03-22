3c690c05f672fc8eb7cfd95643c1e33a38bb5145
Timon <contact@timonmasberg.com>
2023-03-22 20:42:48 +0100

    ci: add e2e runs to main and next-deployment workflows (#101)

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,14 +3,19 @@
  * This is only a minimal backend to get started.
  */
 import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap(): Promise<void> {
 	const app = await NestFactory.create(AppModule, { cors: true });
-	const port = process.env.PORT || 3333;
+	const config = app.get(ConfigService);
+
+	const envPort = config.get('PORT');
+	const port = envPort ? +envPort : 3000;
 	await app.listen(port);
+
 	Logger.log(`🚀 Application is running on: http://localhost:${port}}`);
 }
 
