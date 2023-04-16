dcd4e0f89244b1e7bee8bd932d29f52826a66fdc
Timon Masberg <contact@timonmasberg.com>
2023-04-16 22:59:28 +0200

    fix: migration cli failing without database name

--- tools/db/migrations/cli.ts
+++ tools/db/migrations/cli.ts
@@ -3,6 +3,7 @@ import { mongoMigrateCli } from 'mongo-migrate-ts';
 
 mongoMigrateCli({
 	uri: process.env.MONGODB_URI,
+	database: process.env.MONGODB_DB,
 	migrationsDir: __dirname,
 	migrationsCollection: 'migrations',
 });
