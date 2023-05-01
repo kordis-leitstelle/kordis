df56592e31475e8f3496f4d83a19c5c673849bd6
Timon Masberg <contact@timonmasberg.com>
2023-05-01 21:43:51 +0200

    chore(spa,api): add release version and environment

--- apps/spa/src/environments/environment.ts
+++ apps/spa/src/environments/environment.ts
@@ -2,6 +2,7 @@ import { Environment } from './environment.model';
 
 export const environment: Environment = {
 	production: false,
-	deploymentName: 'Dev Local',
+	environmentName: 'Dev Local',
+	releaseVersion: '0.0.0-development',
 	apiUrl: 'https://localhost:3000',
 };
