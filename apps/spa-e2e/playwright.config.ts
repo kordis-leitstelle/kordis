4656665c01282bd14899b03a1fa0ad9cac4a96b6
Timon Masberg <contact@timonmasberg.com>
2023-10-30 13:05:11 +0100

    chore: migrate to @nx/playwright

--- apps/spa-e2e/playwright.config.ts
+++ apps/spa-e2e/playwright.config.ts
@@ -1,12 +1,11 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 import { devices } from '@playwright/test';
-
-import { baseConfig } from '../../playwright.config.base';
+import { nxE2EPreset } from '@nx/playwright/preset';
 
 const baseURL = process.env.E2E_BASE_URL || 'http://localhost:4200/';
 
 const config: PlaywrightTestConfig = {
-	...baseConfig,
+	...nxE2EPreset(__filename, { testDir: './src' }),
 	projects: [
 		{ name: 'setup', testMatch: /.*\.setup\.ts/ },
 		{
@@ -21,6 +20,7 @@ const config: PlaywrightTestConfig = {
 		baseURL,
 		screenshot: 'only-on-failure',
 		video: 'retry-with-video',
+		trace: 'on-first-retry',
 	},
 };
 
