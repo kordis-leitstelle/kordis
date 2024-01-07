13580397bc8b0454dd2d200333d5d3dff6fb1437
Timon Masberg <contact@timonmasberg.com>
2024-01-07 08:49:05 +0100

    ci(spa): add dockerfile with nginx, implement csp headers (#526)

--- apps/spa/src/app/app.module.ts
+++ apps/spa/src/app/app.module.ts
@@ -4,6 +4,7 @@ import de from '@angular/common/locales/de';
 import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import DOMPurify from 'dompurify';
 import { NZ_I18N, de_DE } from 'ng-zorro-antd/i18n';
 import { firstValueFrom } from 'rxjs';
 
@@ -44,6 +45,26 @@ registerLocaleData(de);
 	providers: [
 		{
 			provide: APP_INITIALIZER,
+			// we need to use a global tt policy here mainly for the ant design icons
+			useFactory: () => async () => {
+				globalThis.trustedTypes.createPolicy('default', {
+					// https://github.com/angular/angular/issues/31329 can't use Angular DomSanitizer here
+					createHTML: (s) => {
+						return DOMPurify.sanitize(
+							s
+								// hack until https://github.com/cure53/DOMPurify/issues/900 figured out
+								.replace('<style />', ''),
+							{
+								USE_PROFILES: { html: true, svg: true },
+							},
+						);
+					},
+				});
+			},
+			multi: true,
+		},
+		{
+			provide: APP_INITIALIZER,
 			useFactory: (http: HttpClient) => async () => {
 				const config = await firstValueFrom(http.get('./assets/config.json'));
 				return Object.assign(environment, { ...config, ...environment });
@@ -51,7 +72,6 @@ registerLocaleData(de);
 			deps: [HttpClient],
 			multi: true,
 		},
-
 		{ provide: NZ_I18N, useValue: de_DE },
 	],
 	bootstrap: [AppComponent],
