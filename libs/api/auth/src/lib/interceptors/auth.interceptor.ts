1c569f267f073907ff02d4e5101f666834864ba2
Timon <contact@timonmasberg.com>
2024-02-01 10:10:29 +0100

    chore(api): add health check endpoint (#650)

--- libs/api/auth/src/lib/interceptors/auth.interceptor.ts
+++ libs/api/auth/src/lib/interceptors/auth.interceptor.ts
@@ -27,6 +27,10 @@ export class AuthInterceptor implements NestInterceptor {
 			req = ctx.getContext<KordisGqlContext>().req;
 		} else {
 			req = context.switchToHttp().getRequest<KordisRequest>();
+
+			if (req.path === '/health-check') {
+				return next.handle();
+			}
 		}
 
 		const possibleAuthUser = this.authUserExtractor.getUserFromRequest(req);
