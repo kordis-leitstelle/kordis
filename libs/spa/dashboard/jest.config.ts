53bb7a6f71f15f360affe691243fa98bb3bcc51c
Jasper Herzberg <jhrzbrg@outlook.com>
2024-02-20 21:13:56 +0100

    test: fix jest display name

--- libs/spa/dashboard/jest.config.ts
+++ libs/spa/dashboard/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-	displayName: 'dashboard',
+	displayName: 'spa-dashboard',
 	preset: '../../../jest.preset.js',
 	setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
 	coverageDirectory: '../../../coverage/libs/spa/dashboard',
