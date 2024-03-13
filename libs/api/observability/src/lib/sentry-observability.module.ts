9745db03f13740b4872066c20eb27b5abbb6b356
Timon Masberg <contact@timonmasberg.com>
2024-03-13 11:49:45 +0100

    fix(api-observability): sentry exception filter logger not provided correctly

--- libs/api/observability/src/lib/sentry-observability.module.ts
+++ libs/api/observability/src/lib/sentry-observability.module.ts
@@ -1,4 +1,4 @@
-import { Logger, Module, OnModuleInit } from '@nestjs/common';
+import { Module, OnModuleInit } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { APP_FILTER, APP_INTERCEPTOR, ModulesContainer } from '@nestjs/core';
 import { init as initSentry } from '@sentry/node';
@@ -7,7 +7,6 @@ import { SentryExceptionsFilter } from './filters/sentry-exceptions.filter';
 import { SentryOTelUserContextInterceptor } from './interceptors/sentry-otel-user-context.interceptor';
 import oTelSDK from './oTelSdk';
 import { KORDIS_LOGGER_SERVICE } from './services/kordis-logger-service.interface';
-import { KordisLogger } from './services/kordis-logger.interface';
 import { SentryLogger } from './services/sentry-logger.service';
 import { wrapProvidersWithTracingSpans } from './trace-wrapper';
 
@@ -30,10 +29,6 @@ import { wrapProvidersWithTracingSpans } from './trace-wrapper';
 	exports: [KORDIS_LOGGER_SERVICE],
 })
 export class SentryObservabilityModule implements OnModuleInit {
-	private readonly logger: KordisLogger = new Logger(
-		SentryObservabilityModule.name,
-	);
-
 	constructor(
 		private readonly config: ConfigService,
 		private readonly modulesContainer: ModulesContainer,
@@ -49,7 +44,5 @@ export class SentryObservabilityModule implements OnModuleInit {
 		});
 		wrapProvidersWithTracingSpans(this.modulesContainer);
 		oTelSDK.start();
-
-		this.logger.log('Sentry initialized');
 	}
 }
