66a6e4815ea6393991f6131243068bebc9c99ac2
Jasper Herzberg <jhrzbrg@outlook.com>
2024-03-13 20:17:01 +0100

    feat: bootstrap protocol SPA component

---
+++ libs/spa/protocol/src/lib/protocol-entry-time/protocol-entry-time.pipe.spec.ts
@@ -0,0 +1,9 @@
+import { ProtocolEntryTimePipe } from './protocol-entry-time.pipe';
+
+// TODO: test ProtocolEntryTimePipe
+describe('ProtocolEntryTimePipe', () => {
+	it('create an instance', () => {
+		const pipe = new ProtocolEntryTimePipe();
+		expect(pipe).toBeTruthy();
+	});
+});
