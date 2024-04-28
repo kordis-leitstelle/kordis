a19de1ccae0416595ecec6da6e82c9bb934cd575
Timon Masberg <contact@timonmasberg.com>
2024-04-28 16:51:42 +0200

    chore(api-test-helpers): mock populate in mongo test helper

--- libs/api/test-helpers/src/lib/mongo.test-helper.ts
+++ libs/api/test-helpers/src/lib/mongo.test-helper.ts
@@ -1,17 +1,39 @@
-import { Model } from 'mongoose';
+import { createMock } from '@golevelup/ts-jest';
+import { ClientSession, Model } from 'mongoose';
+
+import {
+	DbSessionProvider,
+	UNIT_OF_WORK_SERVICE,
+	UnitOfWorkService,
+} from '@kordis/api/shared';
+
+// we need to mock the uow service until jest v30 (https://github.com/jestjs/jest/issues/14874)
+export function uowMockProvider() {
+	return {
+		provide: UNIT_OF_WORK_SERVICE,
+		useValue: createMock<UnitOfWorkService>({
+			asTransaction(
+				work: (uow: DbSessionProvider) => Promise<void>,
+			): Promise<void> {
+				return work({ session: createMock<ClientSession>() });
+			},
+		}),
+	};
+}
 
 export function mockModelMethodResult(
 	model: Model<any>,
 	document: Record<string, any> | null,
-	method: keyof Model<unknown>,
+	method: 'findById' | 'findOne' | 'findOneAndUpdate',
 ) {
-	const findByIdSpy = jest.spyOn(model, method as any);
+	const spy = jest.spyOn(model, method as any);
 
-	findByIdSpy.mockReturnThis().mockReturnValue({
+	spy.mockReturnThis().mockReturnValue({
 		exec: jest.fn().mockReturnValue({
 			...document,
 			toObject: jest.fn().mockReturnValue(document),
 		}),
+		populate: jest.fn().mockReturnThis(),
 		lean: jest.fn().mockReturnValue({
 			...document,
 			toObject: jest.fn().mockReturnValue(document),
@@ -19,5 +41,23 @@ export function mockModelMethodResult(
 		}),
 	} as any);
 
-	return findByIdSpy;
+	return spy;
+}
+
+export function mockModelMethodResults(
+	model: Model<any>,
+	documents: Record<string, any>[],
+	method: 'find' | 'aggregate',
+) {
+	const spy = jest.spyOn(model, method);
+
+	spy.mockReturnThis().mockReturnValue({
+		lean: jest.fn().mockReturnValue({
+			exec: jest.fn().mockReturnValue(documents),
+		}),
+		populate: jest.fn().mockReturnThis(),
+		exec: jest.fn().mockReturnValue(documents),
+	} as any);
+
+	return spy;
 }
