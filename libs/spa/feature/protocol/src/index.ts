9dc76637ab2f307312a846ee8cac233c3d69d9d3
Jasper Herzberg <jhrzbrg@outlook.com>
2024-05-10 15:16:55 +0200

    feat(spa): bootstrap protocol package

--- libs/spa/feature/protocol/src/index.ts
+++ libs/spa/feature/protocol/src/index.ts
@@ -1,1 +1,5 @@
 export * from './lib/components/protocol-view/protocol-view.component';
+export * from './lib/protocol/protocol.component';
+export * from './lib/protocol/protocol.model';
+
+// TODO: evaluate packaging - one library for all feature components?
--- libs/spa/view/dashboard/src/lib/dashboard.component.ts
+++ libs/spa/view/dashboard/src/lib/dashboard.component.ts
@@ -13,7 +13,9 @@ import { NzModalModule, NzModalService } from 'ng-zorro-antd/modal';
 import { AUTH_SERVICE } from '@kordis/spa/core/auth';
 import { TraceComponent } from '@kordis/spa/core/observability';
 import { DeploymentsComponent } from '@kordis/spa/feature/deployment';
-import { ProtocolViewComponent } from '@kordis/spa/feature/protocol';
+import { ProtocolComponent, ProtocolViewComponent } from '@kordis/spa/feature/protocol';
+
+import { ProtocolMockData } from './protocol-mock-data';
 
 @Component({
 	selector: 'krd-dashboard-view',
@@ -29,6 +31,7 @@ import { ProtocolViewComponent } from '@kordis/spa/feature/protocol';
 		NzModalModule,
 		ProtocolViewComponent,
 		DeploymentsComponent,
+		ProtocolComponent,
 	],
 	templateUrl: './dashboard.component.html',
 	styleUrl: './dashboard.component.css',
@@ -39,6 +42,9 @@ export class DashboardComponent {
 	private readonly authService = inject(AUTH_SERVICE);
 	readonly user$ = this.authService.user$;
 	private readonly modal = inject(NzModalService);
+
+	// TODO: load protocol entries from backend
+	public protocolEntries = ProtocolMockData;
 
 	logout(): void {
 		this.authService.logout();
