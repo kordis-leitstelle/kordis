2cbcf48e43ca2d420f96773125e946fa90b1d57a
Jasper Herzberg <jhrzbrg@outlook.com>
2024-06-12 14:23:04 +0200

    test(api-protocol): test createCommunicationMessageCommand for passthrough

--- libs/api/protocol/src/lib/core/command/create-communication-message.command.spec.ts
+++ libs/api/protocol/src/lib/core/command/create-communication-message.command.spec.ts
@@ -1,10 +1,15 @@
 import { createMock } from '@golevelup/ts-jest';
 import { EventBus } from '@nestjs/cqrs';
+import { plainToInstance } from 'class-transformer';
 
 import { AuthUser } from '@kordis/shared/model';
 
+import { UserProducer } from '../entity/partials/producer-partial.entity';
 import { UnknownUnit } from '../entity/partials/unit-partial.entity';
-import { CommunicationMessage } from '../entity/protocol-entries/communication-message.entity';
+import {
+	CommunicationMessage,
+	CommunicationMessagePayload,
+} from '../entity/protocol-entries/communication-message.entity';
 import { ProtocolEntryCreatedEvent } from '../event/protocol-entry-created.event';
 import { ProtocolEntryRepository } from '../repository/protocol-entry.repository';
 import {
@@ -33,25 +38,46 @@ describe('CreateCommunicationMessageCommand', () => {
 		sender.name = 'Bob';
 		const recipient = new UnknownUnit();
 		recipient.name = 'Alice';
+		const time = new Date('1913-10-19T00:00:00');
+		const message = '🛥️';
+		const channel = 'D';
+		const authUser = {
+			id: 'user-id',
+			organizationId: 'org-id',
+			firstName: 'John',
+			lastName: 'Doe',
+		} as unknown as AuthUser;
+
 		const command = new CreateCommunicationMessageCommand(
-			new Date('1913-10-19T00:00:00'),
+			time,
 			sender,
 			recipient,
-			'🛥️',
-			'D',
-			{ id: 'user-id', organizationId: 'org-id' } as unknown as AuthUser,
+			message,
+			channel,
+			authUser,
 		);
 
-		const commMsg = { id: 'entry-id' } as unknown as CommunicationMessage;
-		repositoryMock.create.mockResolvedValueOnce(commMsg);
-
-		const eventPublishSpy = jest.spyOn(eventBusMock, 'publish');
+		const expectedCommMsg = plainToInstance(CommunicationMessage, {
+			sender,
+			recipient,
+			time,
+			searchableText: message,
+			channel,
+			payload: plainToInstance(CommunicationMessagePayload, { message }),
+			producer: plainToInstance(UserProducer, {
+				userId: authUser.id,
+				firstName: authUser.firstName,
+				lastName: authUser.lastName,
+			}),
+			orgId: authUser.organizationId,
+		});
+		repositoryMock.create.mockResolvedValueOnce(expectedCommMsg);
 
 		await handler.execute(command);
 
-		expect(repositoryMock.create).toHaveBeenCalled();
-		expect(eventPublishSpy).toHaveBeenCalledWith(
-			new ProtocolEntryCreatedEvent('org-id', commMsg),
+		expect(repositoryMock.create).toHaveBeenCalledWith(expectedCommMsg);
+		expect(eventBusMock.publish).toHaveBeenCalledWith(
+			new ProtocolEntryCreatedEvent('org-id', expectedCommMsg),
 		);
 	});
 });
