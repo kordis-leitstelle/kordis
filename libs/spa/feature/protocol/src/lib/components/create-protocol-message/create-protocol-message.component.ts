4f7b76c154489d21a2226d5a1119b76dc8965d46
Jasper Herzberg <jhrzbrg@outlook.com>
2025-02-15 19:23:23 +0100

    feat: add protocol create form

--- libs/spa/feature/protocol/src/lib/components/create-protocol-message/create-protocol-message.component.ts
+++ libs/spa/feature/protocol/src/lib/components/create-protocol-message/create-protocol-message.component.ts
@@ -24,55 +24,116 @@ import { MutationCreateCommunicationMessageArgs } from '@kordis/shared/model';
 		NzSelectModule,
 		NzButtonModule,
 	],
-	template: ` <form
-		nz-form
-		[nzLayout]="'inline'"
-		[formGroup]="validateForm"
-		(ngSubmit)="submitForm()"
-	>
-		<nz-form-item>
-			<nz-form-control nzErrorTip="Absender benötigt!">
-				<input formControlName="sender" nz-input required placeholder="Von" />
-			</nz-form-control>
-		</nz-form-item>
-		<nz-form-item>
-			<nz-form-control nzErrorTip="Empfänger benötigt!">
-				<input formControlName="recipient" nz-input required placeholder="An" />
-			</nz-form-control>
-		</nz-form-item>
-		<nz-form-item>
-			<nz-form-control nzErrorTip="Nachricht benötigt!">
-				<input
-					formControlName="message"
-					nz-input
-					required
-					placeholder="Nachricht"
-				/>
-			</nz-form-control>
-		</nz-form-item>
-		<nz-form-item>
-			<nz-form-control ngErrorTip="Kanal benötigt">
-				<nz-select formControlName="channel">
-					<nz-option nzValue="1" nzLabel="DLRG-Kanal 1"></nz-option>
-					<nz-option nzValue="2" nzLabel="DLRG-Kanal 2"></nz-option>
-					<nz-option nzValue="3" nzLabel="DLRG-Kanal 3"></nz-option>
-					<nz-option nzValue="D" nzLabel="Digital"></nz-option>
-					<nz-option nzValue="T" nzLabel="Telefon"></nz-option>
-				</nz-select>
-			</nz-form-control>
-		</nz-form-item>
-		<nz-form-item>
-			<nz-form-control>
-				<button nz-button nzType="primary" [disabled]="!validateForm.valid">
-					Eintragen
-				</button>
-			</nz-form-control>
-		</nz-form-item>
-	</form>`,
-	styles: ``,
+	template: `
+		<div style="background-color: green">
+			<form
+				nz-form
+				[nzLayout]="'inline'"
+				[formGroup]="validateForm"
+				(ngSubmit)="submitForm()"
+				class="create-form"
+			>
+				<nz-form-item>
+					<nz-form-control nzErrorTip="Absender benötigt!">
+						<input
+							formControlName="sender"
+							nz-input
+							required
+							placeholder="Von"
+						/>
+					</nz-form-control>
+				</nz-form-item>
+				<nz-form-item>
+					<nz-form-control nzErrorTip="Empfänger benötigt!">
+						<input
+							formControlName="recipient"
+							nz-input
+							required
+							placeholder="An"
+						/>
+					</nz-form-control>
+				</nz-form-item>
+				<nz-form-item class="message">
+					<nz-form-control nzErrorTip="Nachricht benötigt!">
+						<input
+							formControlName="message"
+							nz-input
+							required
+							placeholder="Nachricht"
+						/>
+					</nz-form-control>
+				</nz-form-item>
+				<nz-form-item class="channel">
+					<nz-form-control ngErrorTip="Kanal benötigt">
+						<nz-select formControlName="channel">
+							@for (channel of channels; track channel.value) {
+								<nz-option
+									[nzValue]="channel.value"
+									[nzLabel]="channel.label"
+								></nz-option>
+							}
+						</nz-select>
+					</nz-form-control>
+				</nz-form-item>
+				<nz-form-item>
+					<nz-form-control>
+						<button nz-button nzType="primary" [disabled]="!validateForm.valid">
+							Eintragen
+						</button>
+					</nz-form-control>
+				</nz-form-item>
+			</form>
+		</div>
+	`,
+	styles: `
+		.create-form {
+			width: 100%;
+			display: flex;
+
+			.message {
+				flex-grow: 1;
+			}
+
+			.channel {
+				min-width: 10em;
+			}
+
+			:last-child {
+				margin-right: 0;
+			}
+		}
+	`,
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CreateProtocolMessageComponent {
+	// TODO: move to backend
+	channels = [
+		{
+			value: 'D',
+			label: 'Digital',
+			default: true,
+		},
+		{
+			value: 'T',
+			label: 'Telefon',
+			default: false,
+		},
+		{
+			value: '1',
+			label: 'DLRG-Kanal 1',
+			default: false,
+		},
+		{
+			value: '2',
+			label: 'DLRG-Kanal 2',
+			default: false,
+		},
+		{
+			value: '3',
+			label: 'DLRG-Kanal 3',
+			default: false,
+		},
+	];
 	newMessage = output<MutationCreateCommunicationMessageArgs>();
 
 	validateForm: FormGroup<{
@@ -84,7 +145,10 @@ export class CreateProtocolMessageComponent {
 		sender: ['', [Validators.required]],
 		recipient: ['', [Validators.required]],
 		message: ['', [Validators.required]],
-		channel: ['D', [Validators.required]],
+		channel: [
+			this.channels.find((channel) => channel.default)?.value ?? '',
+			[Validators.required],
+		],
 	});
 
 	constructor(private fb: NonNullableFormBuilder) {}
