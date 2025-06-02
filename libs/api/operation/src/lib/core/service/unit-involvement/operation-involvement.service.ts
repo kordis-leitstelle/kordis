2770ed78a91100215b3246a08549574c288de76d
Timon Masberg <contact@timonmasberg.com>
2025-06-02 07:53:31 +0200

    fix(api-operation): emit involvement changed event if unit released

--- libs/api/operation/src/lib/core/service/unit-involvement/operation-involvement.service.ts
+++ libs/api/operation/src/lib/core/service/unit-involvement/operation-involvement.service.ts
@@ -1,7 +1,9 @@
 import { Inject, Injectable } from '@nestjs/common';
+import { EventBus } from '@nestjs/cqrs';
 
 import { DbSessionProvider } from '@kordis/api/shared';
 
+import { OngoingOperationInvolvementsUpdatedEvent } from '../../event/ongoing-operation-involvements-updated.event';
 import { UnitAlreadyInvolvedException } from '../../exceptions/unit-already-involved.exception';
 import {
 	CreateUnitInvolvementDto,
@@ -18,6 +20,7 @@ export class OperationInvolvementService {
 	constructor(
 		@Inject(OPERATION_INVOLVEMENT_REPOSITORY)
 		private readonly involvementsRepository: OperationInvolvementsRepository,
+		private readonly eventBus: EventBus,
 	) {}
 
 	/*
@@ -151,6 +154,7 @@ export class OperationInvolvementService {
 		start: Date,
 		uow?: DbSessionProvider,
 	): Promise<void> {
+		let changedOperationID: string | undefined;
 		// has the unit an active involvement?
 		let involvement =
 			await this.involvementsRepository.findByUnitInvolvementTimeRange(
@@ -170,8 +174,7 @@ export class OperationInvolvementService {
 				start,
 				uow,
 			);
-			//todo: müssen wir hier nicht ein event werfen damit deployments das mitkriegt? oder vielleicht am ende ein recalculate trigger?
-			return;
+			changedOperationID = involvement.operationId;
 		}
 		// has a pending involvement?
 		involvement =
@@ -200,6 +203,13 @@ export class OperationInvolvementService {
 					uow,
 				);
 			}
+			changedOperationID = involvement.operationId;
+		}
+
+		if (changedOperationID) {
+			this.eventBus.publish(
+				new OngoingOperationInvolvementsUpdatedEvent(orgId, changedOperationID),
+			);
 		}
 	}
 
